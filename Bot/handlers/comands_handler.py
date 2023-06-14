206e653a6d77e06e9300af7f566862c1a460e9f5
RevelRies <ayzikov070@mail.ru>
2023-06-14 17:02:07 +0300

    feat: add a keyboard for the main menu

--- Bot/handlers/comands_handler.py
+++ Bot/handlers/comands_handler.py
@@ -1,7 +1,12 @@
+# файлы проекта
+from BotMayakovka_2.Bot.keyboards.main_menu_keyboard import main_menu_keyboard
+
 # импорты aiogram
 from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram import Router
+
+
 
 # роутер (сын диспетчера)
 router = Router()
@@ -9,4 +14,7 @@ router = Router()
 # обработка команды /start
 @router.message(Command(commands=['start']))
 async def hello(message: Message):
-    await message.answer(text='Приветствие')
+    # получам клавиатуру
+    markup = await main_menu_keyboard()
+
+    await message.answer(text='Приветствие', reply_markup=markup)
