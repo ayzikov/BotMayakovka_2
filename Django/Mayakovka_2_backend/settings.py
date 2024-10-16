f57c54fef66da4a00fb3aa5eeab9b4d7a6d74a32
Nikita Ayzikov <110584506+ayzikov@users.noreply.github.com>
2024-10-16 20:32:45 +0300

    Update settings.py

--- Django/Mayakovka_2_backend/settings.py
+++ Django/Mayakovka_2_backend/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#+ejmwg2p-ow38)0ncd3$!=l!j22#(r#qn_19&1e$#w&wc^!4$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 
 
 # Application definition
